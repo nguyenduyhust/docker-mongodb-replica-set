f57e08ab54fb66a6b458933fedb69e09dad7a0d0
nguyenduyhust <nguyenduy.hust@gmail.com>
2023-11-01 13:46:25 +0700

    Replace mongo by mongosh

--- mongo-setup.sh
+++ mongo-setup.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /data/mongo-init.flag ]; then
     echo "Init replicaset"
-    mongo mongodb://mongodb1:27011 mongo-setup.js
+    mongosh mongodb://mongodb1:27011 mongo-setup.js
     touch /data/mongo-init.flag
 else
     echo "Replicaset already initialized"
